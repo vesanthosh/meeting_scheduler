4534a198e8376734345e32b24229b3f6d37288f5
Santhosh Venkatesan <vesanthosh@gmail.com>
2019-09-15 18:14:16 +0200

    - few TYPO fix

--- test/scheduler.test.js
+++ test/scheduler.test.js
@@ -1,7 +1,7 @@
 const scheduler = require('../scheduler/scheduler');
 
-describe('get input and get output', () => {
-    test('Meeting should not be scheduled outside of an office hours.', () => {
+describe('Meeting Scheduler with following test case', () => {
+    test('Meeting should not be scheduled outside of office hours.', () => {
 
         var input = "0900 1730\n\n2011-03-17 10:17:06 EMP001\n\n2011-03-21 07:30 2\n\n2011-03-15 17:29:12 EMP002\n\n2011-03-22 16:00 3\n";
 
@@ -10,7 +10,7 @@ describe('get input and get output', () => {
         expect(scheduler(input)).toEqual(expected_output);
     });
 
-    test('Meeting should not be overlap with already scheduled meeting slot.', () => {
+    test('Meeting should not be overlap with the already scheduled meeting slot.', () => {
 
         var input = "0800 1630\n\n2011-03-17 11:23:45 EMP002\n\n2011-03-21 10:00 1\n\n2011-03-16 10:17:06 EMP001\n\n2011-03-21 09:00 2\n";
 
@@ -25,7 +25,7 @@ describe('get input and get output', () => {
         expect(scheduler(input)).toEqual(expected_output);
     });
 
-    test('All the booking requests should be proccessed in a chronological order.', () => {
+    test('All the booking requests should be processed in chronological order.', () => {
 
         var input = "0900 1730\n\n2011-03-17 10:17:06 EMP001\n\n2011-03-21 11:00 2\n\n2011-03-15 12:34:56 EMP002\n\n2011-03-21 09:00 2\n\n2011-03-17 09:28:23 EMP003\n\n2011-03-21 14:00 2";
 
