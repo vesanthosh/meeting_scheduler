bbcc9c31e9d4c76958f181b64a07c817cdca391d
Santhosh Venkatesan <vesanthosh@gmail.com>
2019-09-15 16:56:07 +0200

    - test case 2

--- test/scheduler.test.js
+++ test/scheduler.test.js
@@ -25,6 +25,23 @@ describe('get input and get output', () => {
         expect(scheduler(input)).toEqual(expected_output);
     });
 
+    test('All the booking requests should be proccessed in a chronological order.', () => {
+
+        var input = "0900 1730\n\n2011-03-17 10:17:06 EMP001\n\n2011-03-21 11:00 2\n\n2011-03-15 12:34:56 EMP002\n\n2011-03-21 09:00 2\n\n2011-03-17 09:28:23 EMP003\n\n2011-03-21 14:00 2";
+
+        var expected_output = [
+            {
+                date: '2011-03-21',
+                schedule: [
+                    '09:00 11:00 EMP002',
+                    '11:00 13:00 EMP001',
+                    '14:00 16:00 EMP003'
+                ]
+            }
+        ];
+        expect(scheduler(input)).toEqual(expected_output);
+    });
+
         var input = "0900 1730\n\n2011-03-17 10:17:06 EMP001\n\n2011-03-21 09:00 2\n\n2011-03-16 12:34:56 EMP002\n\n2011-03-21 09:00 2\n\n2011-03-16 09:28:23 EMP003\n\n2011-03-22 14:00 2\n\n2011-03-17 11:23:45 EMP004\n\n2011-03-22 16:00 1\n\n2011-03-15 17:29:12 EMP005\n\n2011-03-21 16:00 3\n";
 
         var expected_output =
